aa6cad877bb710d72029484067e83dc3e4b8b9bc
Victor Manduca <victor.manduca@hotmail.com>
2020-08-22 17:51:32 -0300

    Creating user-controller

--- src/routes/user/user-controller.js
+++ src/routes/user/user-controller.js
@@ -2,12 +2,12 @@ import models from '../../models'
 
 export async function create(req, res, next) {
   const body = { ...req.body }
-  const user = await models.usuario.create(body)
+  const user = await models.user.create(body)
 
   res.status(201).json(user.dataValues)
 }
 
 export async function all(req, res, next) {
-  const users = await models.usuario.findAll()
+  const users = await models.user.findAll()
   res.status(200).json(users)
 }
