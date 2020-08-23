2172e2e90b6ef91897ebc89e7298be292e8cc66a
Victor Manduca <victor.manduca@hotmail.com>
2020-08-23 17:55:10 -0300

    Fixing connection with database with sequelize

--- src/configs/sequelize.js
+++ src/configs/sequelize.js
@@ -2,8 +2,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 module.exports = {
-  username: process.env.db_user,
   database: process.env.db_name,
+  username: process.env.db_user,
+  password: process.env.db_password,
   host: process.env.db_host,
   port: process.env.db_port,
   dialect: 'mysql',
